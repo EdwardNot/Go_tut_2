e2698e649eacf8f4f9d5f9d979a4f29a09e54bfe
EdwardNot <leonardovan@yandex.ru>
2021-02-06 20:09:54 +0300

    Loop

---
+++ ForLoop/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 0; i < 5; i++ {
+		fmt.Println(i)
+	}
+
+	for i, j := 0, 0; i < 6; i, j = i+1, j+1 {
+		if j == 2 {
+			continue
+		}
+		fmt.Println(i, j)
+		if i == 5 {
+			break
+		}
+	}
+
+	s := []int{1, 2, 3}
+	for key, value := range s {
+		fmt.Println(key, value)
+	}
+}
